b11b83531fe720b7f3212fa394fa17f734f2ab08
Pearl Zeng <zhuzeng@umich.edu>
2019-03-15 23:14:27 -0400

    Initialize project and install dependencies

---
+++ src/Botomatic.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Botomatic() {
+  return <h1>Hello RV!</h1>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Botomatic from "./Botomatic";
+
+ReactDOM.render(<Botomatic />, document.getElementById("root"));
