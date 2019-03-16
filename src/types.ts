bd9bbd31d4c5572021f3f6b10c2e2957e218124a
Pearl Zeng <zhuzeng@umich.edu>
2019-03-16 01:43:41 -0400

    Model the data structure through configuring types

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface IRobotBio {
+  name: string;
+  type: RobotType;
+}
+
+export enum RobotType {
+  Unipedal = "Unipedal",
+  Bipedal = "Bipedal",
+  Quadrupedal = "Quadrupedal",
+  Arachnid = "Arachnid",
+  Radial = "Radial",
+  Aeronautical = "Aeronautical"
+}
+
+export interface IRobot extends IRobotBio {
+  working: boolean;
+  todoTasks: ITask[];
+  currentTask: ITask;
+  completedTasks: ITask[];
+}
+
+export interface ITask {
+  description: string;
+  duration: number;
+}
